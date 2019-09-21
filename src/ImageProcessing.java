8f594d6655d687a589c4868a7b2338eb80918bf8
johnGit92 <gnb.celato@gmail.com>
2019-09-21 07:56:51 +0200

    Initial commit

---
+++ src/ImageProcessing.java
@@ -0,0 +1,12 @@
+
+/**
+ * Class which provides image processing utility methods.
+ * 
+ * @author Giovanbattista
+ *
+ */
+public class ImageProcessing {
+	
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * This is a sample java application that process an image applying multithreading.
+ * 
+ * @author Giovanbattista
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
