15b6d7bbc2e2146ae9b8b37e27b5ea37c898acd8
johnGit92 <gnb.celato@gmail.com>
2019-09-21 09:17:13 +0200

    Added helper methods

--- src/ImageProcessing.java
+++ src/ImageProcessing.java
@@ -1,3 +1,4 @@
+import java.awt.image.BufferedImage;
 
 /**
  * Class which provides image processing utility methods.
@@ -6,7 +7,134 @@
  *
  */
 public class ImageProcessing {
-	
-	
+
+	/**
+	 * Recolors the pixel at the x and y coordinates obtained from the original
+	 * image.
+	 * 
+	 * @param originalImage original image.
+	 * @param resultImage   result image.
+	 * @param x             x coordinate of the pixel to recolor.
+	 * @param y             y coordinate of the pixel to recolor.
+	 */
+	public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
+		int rgb = originalImage.getRGB(x, y);
+
+		int red = getRed(rgb);
+		int green = getGreen(rgb);
+		int blue = getBlue(rgb);
+
+		int newRed;
+		int newGreen;
+		int newBlue;
+
+		if (isShadeOfGray(red, green, blue)) {
+			newRed = Math.min(255, red + 10);
+			newGreen = Math.max(0, green - 80);
+			newBlue = Math.max(0, blue - 20);
+		} else {
+			newRed = red;
+			newGreen = green;
+			newBlue = blue;
+		}
+
+		int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+		setRGB(resultImage, x, y, newRGB);
+
+	}
+
+	public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+		image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+	}
+
+	/**
+	 * Determines if a pixel is a shade of gray. Check if all the three components
+	 * have similar color intensity, with no one stronger than the rest.
+	 * 
+	 * @param red   red value.
+	 * @param green green value.
+	 * @param blue  blue value.
+	 * @return return true if a pixel with the specified color values is a shade of
+	 *         gray, false otherwise.
+	 */
+	public static boolean isShadeOfGray(int red, int green, int blue) {
+
+		/**
+		 * Check if the color is almost a perfect mix of green red and blue we know it's
+		 * a shade of gray. 30 is the distance used to determine their proximity.
+		 */
+		return Math.abs(red - green) < 30 && Math.abs(red - blue) < 30 && Math.abs(green - blue) < 30;
+
+	}
+
+	/**
+	 * Build the pixel rgb compound value from the individual value of red, green
+	 * and blue.
+	 * 
+	 * @param red   red value.
+	 * @param green green value.
+	 * @param blue  blue value.
+	 * @return return the rgb compound value from red, green and blue.
+	 */
+	public static int createRGBFromColors(int red, int green, int blue) {
+		int rgb = 0;
+
+		/**
+		 * rgb computed applying the logical OR with the individual values, bit shifting
+		 * properly.
+		 */
+		rgb |= blue; // no shift needed cause blue is the rightmost byte value
+		rgb |= green << 8; // one byte left shift
+		rgb |= red << 16; // two bytes left shift
+
+		rgb |= 0xFF000000; // set the alpha value to the highest value to make the pixel opaque
+
+		return rgb;
+	}
+
+	/**
+	 * Return only the blue value from the specified rgb value.
+	 * 
+	 * @param rgb rgb value.
+	 * @return return the blue value from the specified rgb value.
+	 */
+	public static int getBlue(int rgb) {
+
+		/**
+		 * Apply a bit mask excluding the rightmost byte which is exactly the blue
+		 * component.
+		 */
+		return rgb & 0x000000FF;
+	}
+
+	/**
+	 * Return only the green value from the specified rgb value.
+	 * 
+	 * @param rgb rgb value.
+	 * @return return the green value from the specified rgb value.
+	 */
+	public static int getGreen(int rgb) {
+
+		/**
+		 * Apply a bit mask excluding the alpha, red and blue component and shift 8
+		 * bytes to the right cause green is the second byte from the right.
+		 */
+		return (rgb & 0x0000FF00) >> 8;
+	}
+
+	/**
+	 * Return only the red value from the specified rgb value.
+	 * 
+	 * @param rgb rgb value.
+	 * @return return the red value from the specified rgb value.
+	 */
+	public static int getRed(int rgb) {
+
+		/**
+		 * Apply a bit mask excluding the alpha, green and blue component and shift 16
+		 * bytes to the right cause red is the third byte from the right.
+		 */
+		return (rgb & 0x00FF0000) >> 16;
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 /**
- * This is a sample java application that process an image applying multithreading.
+ * This is a sample java application that process an image applying
+ * multithreading.
  * 
  * @author Giovanbattista
  *
  */
 public class Main {
 
-	public static void main(String[] args) {
+	public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+	public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+		BufferedImage resultimage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+		
+		
 
 	}
 
